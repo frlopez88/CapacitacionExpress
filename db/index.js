21afc8ebfc25d1f1a2940bcc489505bef44ae37d
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-10-24 11:46:32 -0600

    cambio de pass

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const cn = 'postgresql://fernando.lopez:ccolor2021@192.168.20.219:5432/datawarehouse';
+const cn = 'postgresql://user:pass@192.168.20.219:5432/datawarehouse';
 
 const db = pgp(cn);
 
